fa1435b5dbb5cb9650b05ab64eeaa8ac2ac20746
juan-lucs <ls2699120@gmail.com>
2025-09-23 08:15:47 -0300

    Create For.java

---
+++ For.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String[] args) {
+        for(int x = 3; x <=22; x++) {
+            if (x % 2 == 0){
+                System.out.println(x);  
+            }
+        }
+    }
+}
