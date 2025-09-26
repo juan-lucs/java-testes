73ad2f63ddd485054c8aa174454238713f17443d
juan-lucs <ls2699120@gmail.com>
2025-09-26 08:15:19 -0300

    Create Teste3.java

---
+++ Teste3.java
@@ -0,0 +1,8 @@
+public class Teste3 {
+    public static void main(String[] args) {
+        String palavra = "Java";
+        System.out.println("primeira letra " + palavra.charAt(0));
+        System.out.println("segunda letra " + palavra.charAt(1));
+        System.out.println("ultima letra " + palavra.charAt(palavra.length() -1 ) );
+    }
+}
